fc46892031ae5694b63a72ad9ab30f97061bc02e
Wilson Neo <1902725@sit.singaporetech.edu.sg>
2021-07-14 15:59:23 +0800

    Updated venv with own installed libraries. Please REINSTALL the tensorflow package due to github size limitation.

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\danie\Desktop\ITP_NLP\venv"
+$env:VIRTUAL_ENV="C:\Users\Wilson\Documents\ITP\ITP_NLP\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
