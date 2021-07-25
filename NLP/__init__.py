1ea4223405c070de5ed3f6019912a8eafdf4af23
Daniel Tan Jia Wei <1902641@sit.singaporetech.edu.sg>
2021-07-25 19:58:16 +0800

    //Repositioned Upload

--- NLP/__init__.py
+++ NLP/__init__.py
@@ -11,7 +11,7 @@ app.secret_key = "secret key"
 filename = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'static' ,'uploads'))
 UPLOAD_FOLDER = filename
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 100 * 2048 * 2048
 app.config['TRAIN_FOLDER'] = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'static' ,'trains'))
 
 from NLP import routes
